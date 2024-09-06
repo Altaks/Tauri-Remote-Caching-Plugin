33720033dd16607a60c25c10c3374241faaac22d
Altaks <altair61.dev@gmail.com>
2024-09-06 13:20:07 +0200

    feat(project): Made the cache system work

--- tauri-plugin-remote-caching/src/lib.rs
+++ tauri-plugin-remote-caching/src/lib.rs
@@ -1,50 +1,107 @@
+#![feature(async_closure)]
 
-use serde::{ser::Serializer, Serialize};
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD as Base64Encoder, Engine};
+use std::collections::HashMap;
+use std::sync::Arc;
 use tauri::{
-  command,
-  plugin::{Builder, TauriPlugin},
-  AppHandle, Manager, Runtime, State, Window,
+    command,
+    plugin::{Builder, TauriPlugin},
+    AppHandle, Manager, Runtime, State,
 };
-
-use std::{collections::HashMap, sync::Mutex};
-
-type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-  #[error(transparent)]
-  Io(#[from] std::io::Error),
-}
-
-impl Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-  where
-    S: Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
-}
+use tokio::io::AsyncWriteExt;
+use tokio::sync::RwLock;
 
 #[derive(Default)]
-struct MyState(Mutex<HashMap<String, String>>);
+struct CacheState(Arc<RwLock<HashMap<String, String>>>);
 
 #[command]
-async fn execute<R: Runtime>(
-  _app: AppHandle<R>,
-  _window: Window<R>,
-  state: State<'_, MyState>,
-) -> Result<String> {
-  state.0.lock().unwrap().insert("key".into(), "value".into());
-  Ok("success".to_string())
+async fn cached<'a, R: Runtime>(url: String, state: State<'a, CacheState>, app_handle: AppHandle<R>) -> Result<String, String> {
+    let cloned_lock = state.0.clone();
+    let cache_lock = state.0.read().await;
+
+    let url_copy = url.clone();
+
+    match (*cache_lock).get(&url) {
+        Some(value) => {
+            println!("Cache hit");
+            Ok((*value).clone())
+        }
+        None => {
+            println!("Cache miss");
+
+            if let Ok(response) = reqwest::get(url.clone()).await {
+                let url64 = Base64Encoder.encode(url.clone());
+
+                let mime_type = response.headers().get("content-type").unwrap_or_else(|| {
+                    println!("Failed to access content type on response body");
+                    panic!();
+                }).to_str().unwrap();
+
+                let file_format = mime_type.split('/').last().unwrap();
+                let mut save_path = app_handle.path_resolver().resolve_resource(format!("cache/{}", url64.clone())).unwrap();
+                save_path.set_extension(file_format);
+                std::fs::create_dir_all(save_path.clone().parent().unwrap()).expect("Failed to create cache directory");
+                let mut file = tokio::fs::File::create(&save_path).await.expect("Failed to create file");
+
+                let _data = response.bytes().await.unwrap();
+                let data = _data.as_ref();
+
+                if file.write_all(data).await.is_ok() {
+                    file.flush().await.unwrap();
+                    // file has been cached ig
+                    let mut cache_mtx_2 = cloned_lock.write().await;
+                    (&mut *cache_mtx_2).insert(url64, String::from(save_path.to_str().unwrap()));
+                } else {
+                    println!("Failed to write file");
+                }
+            }
+
+            Ok(url_copy)
+        }
+    }
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("remote-caching")
-    .invoke_handler(tauri::generate_handler![execute])
-    .setup(|app| {
-      app.manage(MyState::default());
-      Ok(())
-    })
-    .build()
+    Builder::new("remote-caching")
+        .invoke_handler(tauri::generate_handler![cached])
+        .setup(|app| {
+
+            let cache_dir_path = app.path_resolver().resolve_resource("cache/").unwrap();
+
+            std::fs::create_dir_all(cache_dir_path.clone().parent().unwrap()).expect("Failed to create cache directory");
+            let mut cache_dir = std::fs::read_dir(&cache_dir_path).unwrap();
+
+            let mut cache_registry = HashMap::<String, String>::new();
+
+            while let Some(entry) = cache_dir.next() {
+                if let Ok(entry) = entry {
+                    if let Ok(entry_type) = entry.file_type() {
+                        if entry_type.is_file() {
+                            let path = entry.path();
+
+                            let file_stem = path.file_stem().unwrap().to_str().unwrap();
+                            println!("Found file : {} with extension {:?}", file_stem, path.extension().unwrap().to_str());
+
+                            match Base64Encoder.decode(file_stem) {
+                                Ok(url64) => {
+                                    if let Ok(url) = String::from_utf8(url64) {
+                                        cache_registry.insert(url.clone(), path.to_str().unwrap().to_string());
+                                        println!("Decoded url : {}", url);
+                                    }
+                                }
+                                Err(e) => {
+                                    println!("Failed to decode URL: {}", e);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Inject cache_registry
+            app.manage(CacheState(Arc::new(RwLock::new(cache_registry))));
+            Ok(())
+        })
+        .build()
 }
--- tauri-plugin-remote-caching/webview-src/index.ts
+++ tauri-plugin-remote-caching/webview-src/index.ts
@@ -1,5 +1,8 @@
 import { invoke } from '@tauri-apps/api/tauri'
 
-export async function execute() {
-  await invoke('plugin:remote-caching|execute')
+export async function cached(url: string): Promise<string> {
+  console.log("Invoking cache system for image : " + url);
+  let result = await invoke('plugin:remote-caching|cached', {url: url}) as unknown as string;
+  console.log("Cache system result found : " + url);
+  return result;
 }
